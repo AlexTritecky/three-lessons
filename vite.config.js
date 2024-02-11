ae9bcd8d43726388317e95ec1d8b48ba637bf64f
Alex Triteckiy <alextrytetskyi@Alexs-MacBook-Pro.local>
2024-02-11 14:48:43 +0200

    new dist

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 export default {
     root: 'src/lesson-09/',
-    publicDir: './src/lesson-09/static/textures',
+    publicDir: 'static/textures', // Adjusted to be relative to the root
     base: './',
 
-    server:
-    {
+    server: {
         host: true, // Open to local network and display URL
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-    build:
-    {
-        outDir: './dist', // Output in the dist/ folder
+    build: {
+        outDir: '../dist', // Adjusted to output in the dist folder inside src
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
