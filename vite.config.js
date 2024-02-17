b0dfb9f2dde35bfc01c6180537b8786f74bbe508
Alex Triteckiy <alextrytetskyi@Alexs-MacBook-Pro.local>
2024-02-17 14:16:08 +0200

    add lesson-14

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 export default {
-    root: 'src/lesson-10/',
-    publicDir: 'static/', // Adjusted to be relative to the root
-    base: './',
+  root: "classic_techniques/lesson-14/",
+  publicDir: "static/", // Adjusted to be relative to the root
+  base: "./",
 
-    server: {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
-    },
-    build: {
-        outDir: './dist', // Adjusted to output in the dist folder inside src
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
-}
+  server: {
+    host: true, // Open to local network and display URL
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "./dist", // Adjusted to output in the dist folder inside src
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+  },
+};
